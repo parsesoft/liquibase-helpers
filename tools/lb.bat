7db2eddb1a229cbacf5b8c3fa372bc4f054a78da
Ricardo Stuven <rstuven@gmail.com>
2014-11-27 10:00:05 -0300

    Added support for inserting additional arguments when "status --verbose" is used.

--- tools/lb.bat
+++ tools/lb.bat
@@ -12,6 +12,28 @@ if exist %~dp0..\contexts\%LBL_CONTEXT%\config.bat (
   call %~dp0..\contexts\%LBL_CONTEXT%\config.bat
 )
 
+echo -- Server: %LBL_SERVER%
+echo -- Database: %LBL_DATABASE%
+
+setlocal enabledelayedexpansion
+
+:: Concatenate all arguments except --verbose
+set args=
+for %%a in (%*) do (
+  set last=%%a
+  if [%%a] neq [--verbose] (
+    set args=!args! %%a
+  )
+)
+
+:: Append changelog parameters
+set args=%args% -Ddatabase.name=%LBL_DATABASE%
+
+:: Append --verbose, if needed
+if [%last%] == [--verbose] (
+  set args=%args% %last%
+)
+
 :: (Un)comment line below to toggle verbose execution
 ::echo on
 
@@ -21,6 +43,8 @@ call %~dp0liquibase.bat ^
   --url="jdbc:sqlserver://%LBL_SERVER%;Databasename=%LBL_DATABASE%" ^
   --username=%LBL_USERNAME% ^
   --password=%LBL_PASSWORD% ^
-  %*
+  %args%
+
+endlocal
 
 @echo off
