19cc6c3200dc29948f2de10294afc57575892d5b
Ricardo Stuven <rstuven@gmail.com>
2014-11-27 10:01:39 -0300

    Normalized use of CRLF as scripts EOL.

--- tools/gen_procedure.sql
+++ tools/gen_procedure.sql
@@ -11,23 +11,23 @@ declare @qualified_name varchar(max)
 set @schema_name = SCHEMA_NAME($(schema_id))
 set @qualified_name = '[' + @schema_name + '].[$(object_name)]'
 
-select 
+select
   '--liquibase formatted sql'
-+ CHAR(13)
++ CHAR(13) + CHAR(10)
 + '--http://www.liquibase.org/documentation/sql_format.html'
-+ CHAR(13)
-+ CHAR(13)
++ CHAR(13) + CHAR(10)
++ CHAR(13) + CHAR(10)
 + '--changeset ' + USER + ':PROCEDURE-' + @schema_name + '-$(object_name)'
 + ' runOnChange:true'
 + ' stripComments:false'
 + ' dbms:mssql'
-+ CHAR(13)
-+ CHAR(13) + 'IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N''' + @qualified_name + ''') AND type in (N''P'', N''PC''))'
-+ CHAR(13) + 'DROP PROCEDURE ' + @qualified_name
-+ CHAR(13) + 'GO'
-+ CHAR(13)
-+ CHAR(13) + RTRIM(LTRIM(OBJECT_DEFINITION($(object_id))))
-+ CHAR(13)
-+ CHAR(13) + 'GO'
-+ CHAR(13)
++ CHAR(13) + CHAR(10)
++ CHAR(13) + CHAR(10) + 'IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N''' + @qualified_name + ''') AND type in (N''P'', N''PC''))'
++ CHAR(13) + CHAR(10) + 'DROP PROCEDURE ' + @qualified_name
++ CHAR(13) + CHAR(10) + 'GO'
++ CHAR(13) + CHAR(10)
++ CHAR(13) + CHAR(10) + RTRIM(LTRIM(OBJECT_DEFINITION($(object_id))))
++ CHAR(13) + CHAR(10)
++ CHAR(13) + CHAR(10) + 'GO'
++ CHAR(13) + CHAR(10)
 + '--rollback DROP PROCEDURE ' + @qualified_name
